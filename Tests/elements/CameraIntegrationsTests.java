b49f1b78d329a4073e74b10b3f506f7750621119
itamaroh7 <itamaroh7@gmail.com>
2021-04-16 13:02:56 +0300

    end of level 4

--- Tests/elements/CameraIntegrationsTests.java
+++ Tests/elements/CameraIntegrationsTests.java
@@ -11,6 +11,13 @@ import java.util.List;
 
 public class CameraIntegrationsTests {
 
+    /**
+     *
+     * @param camera camera
+     * @param inter the intersected geometry (grandson of Intersectable Interface)
+     * @param expected the expected intersections
+     * @param testName the name of the current test
+     */
     void testViewPlane(Camera camera, Intersectable inter,int expected,String testName){
         int count=0;
         for(int i=0;i<3;i++){
@@ -24,38 +31,50 @@ public class CameraIntegrationsTests {
     Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)).setVpDistance(1).setVpSize(3,3);
     Camera camera2 = new Camera(new Point3D(0,0,0.5), new Vector(0, 0, -1), new Vector(0, 1, 0)).setVpDistance(1).setVpSize(3,3);
 
+    /**
+     * integration with camera and plane
+     */
     @Test
     void integrationsCameraPlane() {
+        //Plane intersections:
         //EP1 9 intersections
        testViewPlane(camera, new Plane(new Point3D(0,0,-6),new Vector(0,0,1)),9,"Plane EP1");
 
-       //EP2
+       //EP2 9 intersections
         testViewPlane(camera, new Plane(new Point3D(0,0,-6),new Vector(0,1,2)),9,"Plane EP2");
-        //EP3
+        //EP3 6 intersections
        testViewPlane(camera, new Plane(new Point3D(0,0,-6),new Vector(0,1,1)),6,"Plane EP3");
-        //EP4
+        //EP4 0 intersections
         testViewPlane(camera, new Plane(new Point3D(0,0,6),new Vector(0,0,1)),0,"Plane EP4");
     }
+    /**
+     * integration with camera and sphere
+     */
     @Test
     void integrationsCameraSphere(){
-        //EP1
+        //Sphere intersections:
+        //EP1 2 intersections
         testViewPlane(camera,new Sphere(1,new Point3D(0,0,-3)),2,"Sphere EP1");
-
+        //EP2 18 intersections
         testViewPlane(camera2,new Sphere(2.5,new Point3D(0,0,-2.5)),18,"Sphere EP2");
 
-
+        //EP3 10 intersections
         testViewPlane(camera2,new Sphere(2,new Point3D(0,0,-2)),10,"Sphere EP3");
-
+        //EP4 9 intersections
         testViewPlane(camera,new Sphere(4,new Point3D(0,0,-1)),9,"Sphere EP4");
-
+        //EP5 0 intersections
         testViewPlane(camera,new Sphere(0.5,new Point3D(0,0,1)),0,"Sphere EP5");
 
     }
+    /**
+     * integration with camera and triangle
+     */
     @Test
     void integrationsCameraTriangle(){
-
+        //Triangle intersections:
+        //EP1 1 intersections
         testViewPlane(camera,new Triangle(new Point3D(0,1,-2), new Point3D(1,-1,-2), new Point3D(-1,-1,-2)),1,"Triangle EP1");
-
+        //EP2 2 intersections
         testViewPlane(camera,new Triangle(new Point3D(0,20,-2), new Point3D(1,-1,-2), new Point3D(-1,-1,-2)),2,"Triangle EP2");
 
     }
--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -7,6 +7,12 @@ public class Camera {
     final Vector vUp,vTo,vRight;
     double width,height,distance;
 
+    /**
+     * ctor
+     * @param _p0   point p0
+     * @param _vTo  vector to
+     * @param _vUp  vector up
+     */
     public Camera(Point3D _p0, Vector _vTo, Vector _vUp) {
         p0 = _p0;
         vUp = _vUp.normalized();
@@ -17,45 +23,91 @@ public class Camera {
         vRight=vTo.crossProduct(vUp);
     }
 
+    /**
+     *
+     * @return po
+     */
     public Point3D getP0() {
         return p0;
     }
 
+    /**
+     *
+     * @return vetor up
+     */
     public Vector getvUp() {
         return vUp;
     }
 
+    /**
+     *
+     * @return vector to
+     */
     public Vector getvTo() {
         return vTo;
     }
 
+    /**
+     *
+     * @return vector right
+     */
     public Vector getvRight() {
         return vRight;
     }
 
+    /**
+     *
+     * @return width
+     */
     public double getWidth() {
         return width;
     }
 
+    /**
+     *
+     * @return height
+     */
     public double getHeight() {
         return height;
     }
-
+    /**
+     *
+     * @return distance
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     *
+     * @param _width of the view plane
+     * @param _height of the view plane
+     * @return this camera with new view plane width and height
+     */
     public Camera setVpSize(double _width, double _height){
         this.height=_height;
         this.width=_width;
         return this;
     }
 
+    /**
+     *
+     * @param _distance of the view plane
+     * @return  this camera with new view plane distance
+     */
     public Camera setVpDistance(double _distance){
         this.distance=_distance;
         return this;
     }
 
+    /**
+     *
+     * @param nX amount of  columns in view plane
+     * @param nY amount of  rows in view plane
+     * @param j the column index of the pixel
+     * @param i the row index of the pixel
+     * @return ray of specific pixel
+     */
     public Ray constructRayThroughPixel(int nX, int nY, int j, int i){
         Point3D pc=p0.add(vTo.scale(distance));
 
